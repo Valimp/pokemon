34c98afb6b33dbe9ea5b20c37c93410bcd0357ab
kylliancreppy <kyllian.creppy@outlook.fr>
2024-05-03 11:16:32 +0200

    Page accueil et selection maps

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { Routes, Route } from "react-router-dom"
 
+
 import Home from "./pages/Home"
+import HomeMap from "./pages/HomeMap"
+import Accueil from "./pages/Accueil"
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/maps" element={<HomeMap />} />
+      <Route path="/accueil" element={<Accueil />} />
       <Route path="*" element={<div>Not Found</div>} />
     </Routes>
   )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,61 @@
+import CharactersButton from "../components/CharactersButton"
+import { useState } from "react"
+import Pickachu from "../assets/sprites/pikachu.png"
+import '../styles/components/App.scss'
+import StartFightButton from "../components/StartFightButton"
+import characters from "../data/characters"
+
+
 const Home = () => {
+
+    const [initCombat, setInitCombat] = useState({
+        joueur1: "",
+        joueur2: "",
+        map: ""
+    })
+
+    console.log(initCombat);
+    
   return (
-    <div>Home</div>
+    <div>
+        <h1>Home</h1>
+        <div className="charactersChoice">
+            <div className="redRectangle"></div>
+            {
+                characters.map((character) => (
+                    <CharactersButton 
+                        key={character.name} 
+                        name={character.name} 
+                        image={character.image} 
+                        setInitCombat={setInitCombat}
+                        initCombat={initCombat} 
+                        player="1"
+                    />
+                ))
+            }
+        </div>
+        <div className="charactersChoice">
+            <div className="blueRectangle"></div>
+            {
+                characters.map((character) => (
+                    <CharactersButton 
+                        key={character.name} 
+                        name={character.name} 
+                        image={character.image} 
+                        setInitCombat={setInitCombat}
+                        initCombat={initCombat} 
+                        player="2"
+                    />
+                ))
+            }
+        </div>
+        <div>
+            Vous avez choisi {initCombat.joueur1} contre {initCombat.joueur2}
+        </div>
+        <StartFightButton player1={initCombat.joueur1} player2={initCombat.joueur2} />
+    </div>
   )
 }
+
 
 export default Home
